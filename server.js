ce690e289628961c2f4702c88940c385b694ea33
Agwenchez <agweraenock76@gmail.com>
2019-08-01 17:18:17 +0300

    some debuging

--- server.js
+++ server.js
@@ -1,23 +1,51 @@
-const app = require('express')()
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const app = express();
 const bodyParser = require('body-parser')
 const logger = require('morgan')
+const Riders = require("./models/Riders.js")
 const port = process.env.PORT || 3030
 app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-const mongoose = require('mongoose');
-
-//import rider model
-const Rider = require("./models/Riders");
-
-
-
-
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.post('/insert',(req,res, next)=> {
+//   const {name, plateNumber, sacco}= req.body;
+//   const newRider= new Riders({
+//     name,
+//     plateNumber,
+//     sacco
+//   })
+//   newRider.save()
+//   .then(rider=>res.json(rider))
+//   .catch(err=> res.status(500).json({ succeess: false}));
+// })
 app.post('*', (req, res) => {
-  let {sessionId, serviceCode, phoneNumber, text, name, plate_number, sacco} = req.body
+  let { sessionId, serviceCode, phoneNumber, text } = req.body
   var length = text.split('*').length;
   var txt = text.split('*');
   let initial_selection = txt[0];
+  const credentials = {
+    apiKey: 'c5a9746f05f464e3f88a50a83fbf6309cf57d80e452bcff042057ff0080dd8fc',
+    username: 'nyatindopatrick',
+  }
+  // Initialize the SDK
+  const AfricasTalking = require('africastalking')(credentials);
+  // Get the SMS service
+  const sms = AfricasTalking.SMS;
+  function sendMessage(phone_number, sms_message) {
+    const options = {
+      // Set the numbers you want to send to in international format
+      to: [phone_number],
+      // Set your message
+      message: sms_message
+      // Set your shortCode or senderId
+    }
+    // That’s it, hit send and we’ll take care of the rest
+    sms.send(options)
+      .then(console.log)
+      .catch(console.log);
+  }
   if (text == '') {
     // This is the first request. Note how we start the response with CON
     let response = `CON Welcome to Fika Safe. Please pick an option below
@@ -34,102 +62,72 @@ app.post('*', (req, res) => {
     let response = `CON Enter the motorbike plate number:`
     res.send(response)
   }
-  else if(length === 2){
-     
-    
-    if(initial_selection == '1'){
-      
-       let plate= txt[length - 1];
-       let client_phone_number = phoneNumber;
-      
-       // search rider
-       Rider.findOne({plate_number})
-       .then(result=> {
-         let rider = result;
-         let sms_msg = `Rider ${rider.name} of number plate ${rider.plate_number} is registered with ${rider.sacco}` 
-         
-         //send sms to user if found
-         if(rider) {
-         const credentials = {
-           username: 'Agwenchez',
-           apiKey: '57ab9db440418a57b3ae982f0bc7bd08b8018aebdfed406260d0779e52fe38fe'
-         };
-     
-         // initialize africastalking gateway
-         const africastalking = require('africastalking')(credentials);
-     
-         // sms object of africastalking package
-         const sms = africastalking.SMS;
-     
-         // sending parameters
-         const sending_options = {
-           to: [client_phone_number],
-           message: sms_msg
-         };
-     
-         // send sms
-         sms.send(sending_options)
-           .then(response => {
-             console.log(response);
-             res.send(response);
-           })
-           .catch(error => {
-             console.log(error);
-             res.send(error);
-           });
-         }
-       }).catch((err) => {
-        res.status(500).send({ message: `the rider does not exist ${err}` })
-        console.log(err);
+  else if (length === 2) {
+    // let phone_number = txt[length - 1];
+    let client_phone_number = phoneNumber;
+    let sms_message;
+    if (initial_selection == '1') {
+      // search rider
+      // query from databse
+      // let sms_message = `We are not able to verify the rider information provided.`;
+      let rider_detail = txt[length - 1];
+      console.log(rider_detail);
+      // db manenos
+      Riders.findOne({ plateNumber: rider_detail }).exec().then((result) => {
+          if (result) {
+            let rider = result;
+            sms_message = `Rider ${rider.name} whose number plate: ${rider.plateNumber}is registered with ${rider.sacco}.`;
+            sendMessage(client_phone_number, sms_message);
+          } else { 
+            sms_message = `We are not able to verify the rider information provided.` 
+            sendMessage(client_phone_number, sms_message);
+          }
+        }
+      )
+      .catch(err => {
+        res.status(500).send({ message: `internal server error:${err}` });
+        sms_message = `Nothing to send`;
+        console.log("unable to send SMS - exception");
+        sendMessage(client_phone_number, sms_message);
       });
-
-      };
-      
-      // initialize Africas Talking
-      // send SMS
-      // sendSMS(client_phone_number, sms_message);
-      // send sms back to customer
-      let response='END Thank you for your query. You will receive an SMS shortly';
-      res.send(response);
-    }
-    else if(initial_selection == '2'){
-      // rate rider
-      // Business logic for first level response
-      let response= 'CON Rate a rider on a scale of 1 to 5';
-      // This is a terminal request. Note how we start the response with END
-      
-      res.send(response);
-    }
-  
-  else if(length === 3){
+    };
+    // initialize Africas Talking
+    // send SMS
+    // sendSMS(client_phone_number, sms_message);
+    // send sms back to customer
+    let response = 'END Thank you for your query. You will receive an SMS shortly';
+    res.send(response);
+  }
+  else if (initial_selection == '2') {
+    // rate rider
+    // Business logic for first level response
+    let response = 'CON Rate a rider on a scale of 1 to 5';
+    // This is a terminal request. Note how we start the response with END
+    res.send(response);
+  }
+  else if (length === 3) {
     // let rider_rating = txt[length - 1];
     // let plate_number = txt[length - 2];
     // go to databse get rider information
     // set rating for rider and upate
     // Business logic for first level response
-    let response= 'END Thank you for your feedback.';
+    let response = 'END Thank you for your feedback.';
     // This is a terminal request. Note how we start the response with END
-    
     res.send(response);
   }
-    else {
+  else {
     res.status(400).send('Bad request!')
   }
 })
-
-
-mongoose
-  .connect('mongodb://localhost:27017/sms-app', { 
-    useNewUrlParser: true,
-    useCreateIndex: true
-  }) 
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+mongoose.connect('mongodb+srv://nyatindopatrick:dogobigy97@riders-ecfkm.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    // useMongoClient: true,
+    useNewUrlParser: true
+  }
+).then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+  })
+}).catch(err => {
+  console.log(`unable to connect to databse:${err}`);
 })
-
-
-
-
